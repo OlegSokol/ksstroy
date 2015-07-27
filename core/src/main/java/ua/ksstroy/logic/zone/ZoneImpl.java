0925195ee94b85757bb2a094cbb2a8514255945a
saloed <saloed96@gmail.com>
2015-07-27 22:34:01 +0300

    Anton ZoneImpl gde i kak obyavit?

---
+++ core/src/main/java/ua/ksstroy/logic/zone/ZoneImpl.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+
+public class ZoneImpl implements Zone
+{
+	public String getId()
+	{
+		return zoneData.getId();
+	}
+	
+	public String getName()
+	{
+		return zoneData.getName();
+	}
+		
+	public List<Zone> getAdditional()
+	{
+		return zoneData.getAdditional();
+	}
+
+	public List<Zone> getSurplus()
+	{
+		return zoneData.getSurplus();
+	}
+
+	public Mesure getMesure()
+	{
+		return zoneData.getMesure();
+	}
+
+	public Double getWidth()
+	{
+		return zoneData.getWidth();
+	}
+
+	public Double getHeight()
+	{
+		return zoneData.getHeight();
+	}
+
+	public Double getValue()
+	{
+		return zoneData.getValue();
+	}
+	
+	
+	
+}
