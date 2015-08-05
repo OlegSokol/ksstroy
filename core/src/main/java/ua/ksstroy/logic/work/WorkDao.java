5f69a1222e229a47056d546d351fd5fbf2f20c34
saloed <saloed96@gmail.com>
2015-08-05 14:52:54 +0300

    workKooz

---
+++ core/src/main/java/ua/ksstroy/logic/work/WorkDao.java
@@ -0,0 +1,17 @@
+package ua.ksstroy.logic.work;
+
+import java.util.List;
+
+public interface WorkDao {
+
+	WorkHierarchyData getRootWorkHierarchy(String projectId);
+
+	void addGroupToGroup(String groupName, String parentGroupId);
+
+	void addWork(WorkData work, String parentGroupId);
+
+	void addCoverToWork(CoverData cover, String workId);
+
+	void addAdjustmentToWork(AdjustmentData adjustment, String workId);
+
+}
