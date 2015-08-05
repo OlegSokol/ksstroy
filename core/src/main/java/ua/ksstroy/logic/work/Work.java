1c5d95cf3f99c7e29680fe2c8def572d75816020
KooZ <koozbalda@gmail.com>
2015-08-05 13:01:58 +0300

    interface for work and manager implementation

---
+++ core/src/main/java/ua/ksstroy/logic/work/Work.java
@@ -0,0 +1,48 @@
+package ua.ksstroy.logic.work;
+
+import java.util.List;
+import ua.ksstroy.logic.worktype.WorkTypeData;
+import ua.ksstroy.logic.zone.ZoneData;
+
+public interface Work {
+	
+	public WorkTypeData getType(); 
+
+	public void setType(WorkTypeData type); 
+
+	public List<ZoneData> getWorkZones(); 
+
+	public void setWorkZones(List<ZoneData> workZones); 
+
+	public List<CoverData> getAllCovers(); 
+
+	public void setAllCovers(List<CoverData> allCovers); 
+
+	public String getId(); 
+
+	public void setId(String id); 
+
+	public String getName(); 
+	public void setName(String name); 
+
+	public Double getPlanedCost(); 
+
+	public void setPlanedCost(Double planedCost);
+
+	public Double getPerspectiveCost();
+
+	public void setPerspectiveCost(Double perspectiveCost); 
+
+	public Double getClosedCost(); 
+
+	public void setClosedCost(Double closedCost); 
+
+	public Double getDealCost(); 
+
+	public void setDealCost(Double dealCost); 
+
+	public List<AdjustmentData> getAdjustments(); 
+
+	public void setAdjustments(List<AdjustmentData> adjustments); 
+
+}
---
+++ core/src/main/java/ua/ksstroy/logic/work/WorkManagerImpl.java
@@ -0,0 +1,38 @@
+package ua.ksstroy.logic.work;
+
+import java.util.List;
+
+public class WorkManagerImpl implements WorkManager{
+
+	@Override
+	public WorkHierarchyData getRootWorkHierarchy(String projectId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addGroupToGroup(String groupName, String parentGroupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addWork(WorkData work, String parentGroupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCoverToWork(CoverData cover, String workId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAdjustmentToWork(AdjustmentData adjustment, String workId) {
+		
+		//adjustments.add(adjustment);
+		
+	}
+
+}
