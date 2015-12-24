c073c6725106500a42409256b64d8c528078cb8f
Djakson <evgeniy.sobolev.1995@gmail.com>
2015-12-25 01:21:31 +0200

    Fix script no full hierarhy

--- dao/KsstroyInit.sql
+++ dao/KsstroyInit.sql
@@ -1,36 +1,35 @@
-
-
 DROP TABLE IF EXISTS `projects`;
 CREATE TABLE `projects` (
-  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `PROJECT_NAME` VARCHAR(64) NOT NULL,
-  `DESCRIPTION` VARCHAR(512), 
-  `ID_GROUP` INT(10) UNSIGNED
-);  
-
+  `ID`           INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `PROJECT_NAME` VARCHAR(64)      NOT NULL,
+  `DESCRIPTION`  VARCHAR(512),
+  `ID_GROUP`     INT(10) UNSIGNED
+);
 
 
 INSERT INTO `projects` (`ID`, `PROJECT_NAME`, `DESCRIPTION`, `ID_GROUP`) VALUES (33, 'Cool Project', 'Very cool', '1');
-INSERT INTO `projects` (`ID`, `PROJECT_NAME`, `DESCRIPTION`, `ID_GROUP`) VALUES (34, 'Another Cool Project', 'Super cool', '1');
-INSERT INTO `projects` (`ID`, `PROJECT_NAME`, `DESCRIPTION`, `ID_GROUP`) VALUES (35, 'Not Cool Project', 'Not cool', '1');
+INSERT INTO `projects` (`ID`, `PROJECT_NAME`, `DESCRIPTION`, `ID_GROUP`)
+VALUES (34, 'Another Cool Project', 'Super cool', '1');
+INSERT INTO `projects` (`ID`, `PROJECT_NAME`, `DESCRIPTION`, `ID_GROUP`)
+VALUES (35, 'Not Cool Project', 'Not cool', '1');
 
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `NAME` VARCHAR(64) NOT NULL,
-  `PASSWORD` VARCHAR(64) NOT NULL,
-  `ROLE` VARCHAR(32) NOT NULL
+  `ID`       INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `NAME`     VARCHAR(64)      NOT NULL,
+  `PASSWORD` VARCHAR(64)      NOT NULL,
+  `ROLE`     VARCHAR(32)      NOT NULL
 );
 
 
 INSERT INTO `users` (`ID`, `NAME`, `PASSWORD`, `ROLE`) VALUES (1, 'admin', 'nimda', 'admin');
-  
+
 
 DROP TABLE IF EXISTS `user_project_rel`;
 CREATE TABLE `user_project_rel` (
   `project` INT(10),
-  `user` INT(10)
+  `user`    INT(10)
 );
 INSERT INTO `user_project_rel` (`project`, `user`) VALUES (33, 1);
 INSERT INTO `user_project_rel` (`project`, `user`) VALUES (34, 1);
@@ -38,81 +37,82 @@ INSERT INTO `user_project_rel` (`project`, `user`) VALUES (34, 1);
 
 DROP TABLE IF EXISTS `zones`;
 CREATE TABLE `zones` (
-  `id` BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(50) not NULL ,
-  `width` double unsigned not NULL,
-  `height` double unsigned not NULL ,
-  `mesure_name` VARCHAR(50) not NULL ,
-  `parent_group_id` INT(10) UNSIGNED ,
-  `surplus_for_zones_id` INT(10) UNSIGNED ,#have value only if the zone is surplus
-  `addit_for_zones_id` INT(10) UNSIGNED #have value only if the zone is additional
+  `id`                   BIGINT(10)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`                 VARCHAR(50)     NOT NULL,
+  `width`                DOUBLE UNSIGNED NOT NULL,
+  `height`               DOUBLE UNSIGNED NOT NULL,
+  `mesure_name`          VARCHAR(50)     NOT NULL,
+  `parent_group_id`      INT(10) UNSIGNED,
+  `surplus_for_zones_id` INT(10) UNSIGNED, #have value only if the zone is surplus
+  `addit_for_zones_id`   INT(10) UNSIGNED #have value only if the zone is additional
 );
 
 
-INSERT INTO `zones`  VALUES (1,'pol1', 10.0,10.0,'M2',4,null,null);
-INSERT INTO `zones`  VALUES (2,'stenaA', 6.0,3.0,'M2',5,null,null);
-INSERT INTO `zones`  VALUES (3,'potolok', 10.0,10.0,'M2',5,null,null);
-INSERT INTO `zones`  VALUES (4,'stenaB', 7.0,3.5,'M2',6,null,null);
-INSERT INTO `zones`  VALUES (5,'otkosi1', 5.0,5.0,'M2',null,null,1);
-INSERT INTO `zones`  VALUES (6,'otkosi2', 3.0,2.0,'M2',null,null,2);
-INSERT INTO `zones`  VALUES (7,'pamyatnikLenina', 2.0,2.0,'M2',null,null,3);
-INSERT INTO `zones`  VALUES (8,'pamyatnikDartVeyderu', 10.0,4.0,'M2',null,null,4);
-INSERT INTO `zones`  VALUES (9,'dver1', 2.0,1.0,'M2',null,1,null);
-INSERT INTO `zones`  VALUES (10,'dver2', 3.0,2.0,'M2',null,2,null);
-INSERT INTO `zones`  VALUES (11,'okno1', 3.0,1.25,'M2',null,3,null);
-INSERT INTO `zones`  VALUES (12,'okno2', 2.0,1.35,'M2',null,4,null);
+INSERT INTO `zones` VALUES (1, 'pol1', 10.0, 10.0, 'M2', 4, NULL, NULL);
+INSERT INTO `zones` VALUES (2, 'stenaA', 6.0, 3.0, 'M2', 5, NULL, NULL);
+INSERT INTO `zones` VALUES (3, 'potolok', 10.0, 10.0, 'M2', 5, NULL, NULL);
+INSERT INTO `zones` VALUES (4, 'stenaB', 7.0, 3.5, 'M2', 6, NULL, NULL);
+INSERT INTO `zones` VALUES (5, 'otkosi1', 5.0, 5.0, 'M2', NULL, NULL, 1);
+INSERT INTO `zones` VALUES (6, 'otkosi2', 3.0, 2.0, 'M2', NULL, NULL, 2);
+INSERT INTO `zones` VALUES (7, 'pamyatnikLenina', 2.0, 2.0, 'M2', NULL, NULL, 3);
+INSERT INTO `zones` VALUES (8, 'pamyatnikDartVeyderu', 10.0, 4.0, 'M2', NULL, NULL, 4);
+INSERT INTO `zones` VALUES (9, 'dver1', 2.0, 1.0, 'M2', NULL, 1, NULL);
+INSERT INTO `zones` VALUES (10, 'dver2', 3.0, 2.0, 'M2', NULL, 2, NULL);
+INSERT INTO `zones` VALUES (11, 'okno1', 3.0, 1.25, 'M2', NULL, 3, NULL);
+INSERT INTO `zones` VALUES (12, 'okno2', 2.0, 1.35, 'M2', NULL, 4, NULL);
 
 
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
-    `group_id` BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50) ,
-    `parent_id` BIGINT(20) 
+  `group_id`  BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`      VARCHAR(50),
+  `parent_id` BIGINT(20)
 );
 
-INSERT INTO `groups`  VALUES (1,'Kvartira',null);
-INSERT INTO `groups`  VALUES (2,'Penthaus',null);
-INSERT INTO `groups`  VALUES (3,'Dacha',null);
-INSERT INTO `groups` VALUES (4,'Kuhnya', 1);
-INSERT INTO `groups` VALUES (5,'Spalnya', 1);
-INSERT INTO `groups` VALUES (6,'Tualet', 1);
-INSERT INTO `groups` VALUES (7,'Vannaya', 2);
+INSERT INTO `groups` VALUES (1, 'Kvartira', NULL);
+INSERT INTO `groups` VALUES (2, 'Penthaus', NULL);
+INSERT INTO `groups` VALUES (3, 'Dacha', NULL);
+INSERT INTO `groups` VALUES (4, 'Kuhnya', 1);
+INSERT INTO `groups` VALUES (5, 'Spalnya', 1);
+INSERT INTO `groups` VALUES (6, 'Tualet', 1);
+INSERT INTO `groups` VALUES (7, 'Vannaya', 2);
 
-INSERT INTO `groups` VALUES (8,'Prihozhaya', 2);
+INSERT INTO `groups` VALUES (8, 'Prihozhaya', 2);
 
 
-drop table if exists `worktypes`;
-create table `worktypes` (
-	`worktype_id` BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(50),
-    `description` VARCHAR(512), 
-    `measure_name` VARCHAR(50) not NULL,
-    `unit_price` double,
-    `parent_group_id` INT(10) UNSIGNED
+DROP TABLE IF EXISTS `worktypes`;
+CREATE TABLE `worktypes` (
+  `worktype_id`     BIGINT(10)  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`            VARCHAR(50),
+  `description`     VARCHAR(512),
+  `measure_name`    VARCHAR(50) NOT NULL,
+  `unit_price`      DOUBLE,
+  `parent_group_id` INT(10) UNSIGNED
 );
 
-insert into `worktypes` values (1, 'podgotovka', 'zavarit_doshirak', 'L', 3.45, 2);
-insert into `worktypes` values (2, 'namaz', 'molitva_alahu', 'L', 4.57, 2);
-insert into `worktypes` values (3,  'buy_doshirak', 'go_to_shop_for_doshirak', 'L', 4.57, 1);
+INSERT INTO `worktypes` VALUES (1, 'podgotovka', 'zavarit_doshirak', 'L', 3.45, 3);
+INSERT INTO `worktypes` VALUES (2, 'namaz', 'molitva_alahu', 'L', 4.57, 3);
+INSERT INTO `worktypes` VALUES (3, 'buy_doshirak', 'go_to_shop_for_doshirak', 'L', 4.57, 3);
 
 
-drop table if exists `worktype_material_rel`;
-create table `worktype_material_rel` (
-	`worktype_id` BIGINT(10) NOT NULL,
-    `material_id` BIGINT(10) NOT NULL
+DROP TABLE IF EXISTS `worktype_material_rel`;
+CREATE TABLE `worktype_material_rel` (
+  `worktype_id` BIGINT(10) NOT NULL,
+  `material_id` BIGINT(10) NOT NULL
 );
 
 INSERT INTO `worktype_material_rel` (`worktype_id`, `material_id`) VALUES (1, 1);
 INSERT INTO `worktype_material_rel` (`worktype_id`, `material_id`) VALUES (2, 2);
 
 
-drop table if exists `worktype_groups`;
-create table `worktype_groups` (
-	`group_id` BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(50),
-    `parent_id` bigint(20)
+DROP TABLE IF EXISTS `worktype_groups`;
+CREATE TABLE `worktype_groups` (
+  `group_id`  BIGINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name`      VARCHAR(50),
+  `parent_id` BIGINT(20)
 );
 
-insert into `worktype_groups` values (1, 'root_group', null);
-insert into `worktype_groups` values (2, 'group_one', 1);
-insert into `worktype_groups` values (3, 'group_two', 2);
+INSERT INTO `worktype_groups` VALUES (1, 'root_group', NULL);
+INSERT INTO `worktype_groups` VALUES (2, 'group_one', 1);
+INSERT INTO `worktype_groups` VALUES (3, 'group_two', 2);
+INSERT INTO `worktype_groups` VALUES (3, 'group_three', 2);
