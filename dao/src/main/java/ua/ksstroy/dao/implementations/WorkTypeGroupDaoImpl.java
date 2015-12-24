65131af9bd340e2ab6cc93a227879fe572271d22
Djakson <evgeniy.sobolev.1995@gmail.com>
2015-12-25 01:08:29 +0200

    Wiev full hierarhy. FIX

--- dao/src/main/java/ua/ksstroy/dao/implementations/WorkTypeGroupDaoImpl.java
+++ dao/src/main/java/ua/ksstroy/dao/implementations/WorkTypeGroupDaoImpl.java
@@ -1,16 +1,9 @@
 package ua.ksstroy.dao.implementations;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Component;
-
 import ua.ksstroy.logic.worktype.WorkTypeData;
-import ua.ksstroy.logic.worktype.WorkTypeGroup;
 import ua.ksstroy.logic.worktype.WorkTypeGroupDao;
 import ua.ksstroy.logic.worktype.WorkTypeGroupData;
-import ua.ksstroy.logic.zone.ZoneGroup;
-import ua.ksstroy.models.project.ProjectModel;
 import ua.ksstroy.models.worktype.WorkTypeGroupModel;
 import ua.ksstroy.models.worktype.WorkTypeModel;
 import ua.ksstroy.persistence.DoInTransaction;
@@ -18,87 +11,78 @@ import ua.ksstroy.persistence.GetInTransaction;
 import ua.ksstroy.persistence.SessionWrapper;
 import ua.ksstroy.persistence.TransactionHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class WorkTypeGroupDaoImpl implements WorkTypeGroupDao {
-	private TransactionHelper helper = new TransactionHelper();
+    private TransactionHelper helper = new TransactionHelper();
 
-	@Override
-	public void addWorkTypeGroup(final String workTypeGroupName, final String parentGroupId) {
-		helper.doWithCommit(new DoInTransaction() {
-			@Override
-			public void process(SessionWrapper session) {
-				WorkTypeGroupModel model = new WorkTypeGroupModel();
-				model.setName(workTypeGroupName);
-				model.setSubGroupsToRootGroup(session.get(WorkTypeGroupModel.class, parentGroupId));
-				session.save(model);
-			}
-		});
-		
-	}
+    @Override
+    public void addWorkTypeGroup(final String workTypeGroupName, final String parentGroupId) {
+        helper.doWithCommit(new DoInTransaction() {
+            @Override
+            public void process(SessionWrapper session) {
+                WorkTypeGroupModel model = new WorkTypeGroupModel();
+                model.setName(workTypeGroupName);
+                model.setSubGroupsToRootGroup(session.get(WorkTypeGroupModel.class, parentGroupId));
+                session.save(model);
+            }
+        });
 
-	@Override
-	public void addWorkTypeGroup(final String groupName) {
-		helper.doWithCommit(new DoInTransaction() {
-			@Override
-			public void process(SessionWrapper session) {
-				WorkTypeGroupModel model = new WorkTypeGroupModel();
-				model.setName(groupName);
-				session.save(model);
-			}
-		});
-		
-	}
+    }
 
-	@Override
-	public void removeWorkTypeGroup(final String groupId) {
-		helper.doWithCommit(new DoInTransaction() {
-			@Override
-			public void process(SessionWrapper session) {
-				session.delete(session.get(WorkTypeGroupModel.class, groupId));
-			}
-		});
-		
-	}
+    @Override
+    public void addWorkTypeGroup(final String groupName) {
+        helper.doWithCommit(new DoInTransaction() {
+            @Override
+            public void process(SessionWrapper session) {
+                WorkTypeGroupModel model = new WorkTypeGroupModel();
+                model.setName(groupName);
+                session.save(model);
+            }
+        });
 
-	@Override
-	public WorkTypeGroupData getWorkTypeHierarchy() {
-		return helper.simpleAction(new GetInTransaction<WorkTypeGroupData>() {
-			public WorkTypeGroupData process(SessionWrapper session) {
-				 WorkTypeGroupData workTypeHierarchy = convertWorkTypeGroupModelToData(session.get(WorkTypeGroupModel.class, "1"));
-				return workTypeHierarchy;
-			}
-		});
-	}
-	
-	public WorkTypeGroupData convertWorkTypeGroupModelToData(WorkTypeGroupModel model) {
-		WorkTypeGroupData data = new WorkTypeGroupData();
-		data.setId(Integer.parseInt(model.getId()));
-		data.setName(model.getName());
-		
-		List<WorkTypeGroupData> workTypeGroupList = new ArrayList<>();
-		if (model.getSubGroups() == null) {
-			
-		} else {
-			for(WorkTypeGroupModel group : model.getSubGroups()) {
-				workTypeGroupList.add(convertWorkTypeGroupModelToData(group));
-			}
-			data.setWorkTypeGroupsData(workTypeGroupList);
-		}
-		
-		/*data.setWorkTypeGroupsData(workTypeGroupList);*/
-		
-		List<WorkTypeData> workTypeDatas = new ArrayList<>();
-		if (model.getWorkTypeGroup() == null) {
-			
-		} else {
-			for (WorkTypeModel workTypeModel : model.getWorkTypeGroup()) {
-				workTypeDatas.add(new WorkTypeDaoImpl().convertModelToWorkTypeData(workTypeModel));
-			}
-			data.setWorkTypesData(workTypeDatas);
-		}	
-		/*data.setWorkTypesData(workTypeDatas);*/
-		
-		return data;
-		
-	}
+    }
+
+    @Override
+    public void removeWorkTypeGroup(final String groupId) {
+        helper.doWithCommit(new DoInTransaction() {
+            @Override
+            public void process(SessionWrapper session) {
+                session.delete(session.get(WorkTypeGroupModel.class, groupId));
+            }
+        });
+
+    }
+
+    @Override
+    public WorkTypeGroupData getWorkTypeHierarchy() {
+        return helper.simpleAction(new GetInTransaction<WorkTypeGroupData>() {
+            public WorkTypeGroupData process(SessionWrapper session) {
+                WorkTypeGroupData workTypeHierarchy = convertWorkTypeGroupModelToData(session.get(WorkTypeGroupModel.class, "1"));
+                return workTypeHierarchy;
+            }
+        });
+    }
+
+    public WorkTypeGroupData convertWorkTypeGroupModelToData(WorkTypeGroupModel model) {
+        WorkTypeGroupData data = new WorkTypeGroupData();
+        data.setId(Integer.parseInt(model.getId()));
+        data.setName(model.getName());
+
+        List<WorkTypeGroupData> workTypeGroupList = new ArrayList<>();
+        for (WorkTypeGroupModel group : model.getSubGroups()) {
+            workTypeGroupList.add(convertWorkTypeGroupModelToData(group));
+        }
+        data.setWorkTypeGroupsData(workTypeGroupList);
+
+        List<WorkTypeData> workTypeData = new ArrayList<>();
+        for (WorkTypeModel workTypeModel : model.getWorkTypeGroup()) {
+            workTypeData.add(new WorkTypeDaoImpl().convertModelToWorkTypeData(workTypeModel));
+        }
+        data.setWorkTypesData(workTypeData);
+
+        return data;
+    }
 }
