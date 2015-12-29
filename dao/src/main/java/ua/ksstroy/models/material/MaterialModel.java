3a1c369d3b9c3440955b30a36e4c229ffc4df90b
Djakson <evgeniy.sobolev.1995@gmail.com>
2015-12-29 16:47:43 +0200

    Add full material models

--- dao/src/main/java/ua/ksstroy/models/material/MaterialModel.java
+++ dao/src/main/java/ua/ksstroy/models/material/MaterialModel.java
@@ -1,11 +1,124 @@
 package ua.ksstroy.models.material;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "materials", catalog = "ksstroy")
 public class MaterialModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "measure_name")
+    private String measureName;
+
+    @Column(name = "size")
+    private Double size;
+
+    @Column(name = "planed_cost")
+    private Double planedCost;
+
+    @Column(name = "deal_cost")
+    private Double dealCost;
+
+    @Column(name = "closed_cost")
+    private Double closedCost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private MaterialTypeModel materialTypeModel;
+
+    public MaterialModel(String name, String description, String measureName, Double size, Double planedCost, Double dealCost, Double closedCost) {
+        this.name = name;
+        this.description = description;
+        this.measureName = measureName.toString();
+        this.size = size;
+        this.planedCost = planedCost;
+        this.dealCost = dealCost;
+        this.closedCost = closedCost;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMeasureName() {
+        return measureName;
+    }
+
+    public void setMeasureName(String measureName) {
+        this.measureName = measureName;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Double getPlanedCost() {
+        return planedCost;
+    }
+
+    public void setPlanedCost(Double planedCost) {
+        this.planedCost = planedCost;
+    }
+
+    public Double getDealCost() {
+        return dealCost;
+    }
+
+    public void setDealCost(Double dealCost) {
+        this.dealCost = dealCost;
+    }
+
+    public Double getClosedCost() {
+        return closedCost;
+    }
+
+    public void setClosedCost(Double closedCost) {
+        this.closedCost = closedCost;
+    }
+
+    public MaterialTypeModel getMaterialTypeModel() {
+        return materialTypeModel;
+    }
+
+    public void setMaterialTypeModel(MaterialTypeModel materialTypeModel) {
+        this.materialTypeModel = materialTypeModel;
+    }
 
 }
--- dao/src/main/java/ua/ksstroy/models/material/MaterialTypeModel.java
+++ dao/src/main/java/ua/ksstroy/models/material/MaterialTypeModel.java
@@ -1,11 +1,66 @@
 package ua.ksstroy.models.material;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "material_type", catalog = "ksstroy")
 public class MaterialTypeModel implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_id")
+    @OrderBy
+    private Set<MaterialModel> materialTypeToMaterial = new LinkedHashSet<>();
+
+    public MaterialTypeModel(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<MaterialModel> getMaterialTypeToMaterial() {
+        return materialTypeToMaterial;
+    }
+
+    public void setMaterialTypeToMaterial(Set<MaterialModel> materialTypeToMaterial) {
+        this.materialTypeToMaterial = materialTypeToMaterial;
+    }
 }
