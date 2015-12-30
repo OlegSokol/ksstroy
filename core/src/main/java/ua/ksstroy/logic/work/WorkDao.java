91bb647654ed86c873dd163ba63ac6355791b85a
Soklakov <Sokol@mail.com>
2015-12-30 13:12:41 +0200

    update core for Work

--- core/src/main/java/ua/ksstroy/logic/work/WorkDao.java
+++ core/src/main/java/ua/ksstroy/logic/work/WorkDao.java
@@ -6,12 +6,26 @@ public interface WorkDao {
 
 	WorkHierarchyData getRootWorkHierarchy(String projectId);
 
-	void addGroupToGroup(String groupName, String parentGroupId);
+	/*
+	 * actions from Works
+	 */
 
 	void addWork(Work work, String parentGroupId);
 
 	void addCoverToWork(Cover cover, String workId);
 
 	void addAdjustmentToWork(Adjustment adjustment, String workId);
+
+	/*
+	 * actions from Groups
+	 */
+
+	void addRootGroup(String groupName, Integer projectId);
+
+	void addGroupToGroup(String groupName, String parentGroupId);
+
+	void updateGroup(String groupId, String newGroup);
+
+	void removeGroup(String groupId);
 
 }
